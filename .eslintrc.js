c7b488211250923ad7cc8d5710b11a9a625f4387
itsalysialynn <itsalysialynn@gmail.com>
2018-10-05 10:50:31 -0700

    add eslint rules and gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "plugins": [
+      "react",
+      "jsx-a11y",
+      "import"
+  ],
+  "rules": {
+    "max-len": ["error", 120],
+    "react/jsx-closing-bracket-location": [1, {selfClosing: 'after-props', nonEmpty: 'after-props'}],
+    "react/destructuring-assignment": 0,
+    "object-curly-newline": 0,
+    "react/sort-comp": [1, { order: [
+      'static-methods',
+      'lifecycle',
+      'render',
+      'everything-else',
+    ]}],
+  },
+  "root": true,
+};
