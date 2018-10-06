8b74b78f9aff4634f892df4031f5ff945af6a063
itsalysialynn <itsalysialynn@gmail.com>
2018-10-05 22:13:48 -0700

    added comma seperated currency

--- src/helpers/numberConversionHelper.js
+++ src/helpers/numberConversionHelper.js
@@ -1,10 +1,14 @@
 import round from 'lodash/round'
 
+const addCommas = (amount) => {
+  return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 export default {
   toCurrency: (amount) => {
     if (!amount || amount === '') { return null }
     if (isNaN(amount)) { return 0.00 }
-    if (amount < 0) { return `($${round(amount, 2).toFixed(2).replace("-","")})` }
-    return `$${round(amount, 2).toFixed(2)}`
+    if (amount < 0) { return `($${addCommas(round(amount, 2).toFixed(2).replace('-',''))})` }
+    return `$${addCommas(round(amount, 2).toFixed(2))}`
   },
 }
